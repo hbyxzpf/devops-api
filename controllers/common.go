46c19f38491134f44adb111d7d8bbb8c69e613ff
zhaopengfei <zhaopengfei@huatu.com>
2020-10-23 11:25:52 +0800

    fix go.mod

--- controllers/common.go
+++ controllers/common.go
@@ -105,15 +105,8 @@ func (b *BaseController) Prepare() {
 
 	uniqueID := b.Ctx.Input.Header(UniQueIDName)
 	if uniqueID == "" {
-		uid, err := uuid.NewV4()
-		if err != nil {
-			common.GetLogger().Error(map[string]interface{}{
-				"entryType": "Get UUID",
-			}, fmt.Sprintf("%s", err))
-			uniqueID = ""
-		} else {
-			uniqueID = fmt.Sprintf("%s", uid)
-		}
+		uid := uuid.NewV4()
+		uniqueID = fmt.Sprintf("%s", uid)
 	}
 	b.Data[UniQueIDName] = uniqueID
 
