b969dd3bb9f62556673db351c6ffd0a2ba726ffa
zhaopengfei <hbyx.zpf@gmail.com>
2020-11-05 19:30:16 +0800

    fix some

--- controllers/proxy.go
+++ controllers/proxy.go
@@ -13,9 +13,9 @@ func (p *ProxyController) Reverse() {
 	module := fmt.Sprintf("proxy::%s",p.Ctx.Input.Param(":module"))
 	body,err := common.Reverse(p.Ctx.Input,module)
 	if err != nil{
-		p.JsonError(proxyEntryType,err.Error(),StringMap{"result":StringMap{"success":"fail"}},true)
+		p.JsonError(proxyEntryType,err.Error(),StringMap{"success":"fail"},true)
 	}
-	p.JsonOK(proxyEntryType,StringMap{"result":StringMap{"success":"success","body":body}},true)
+	p.JsonOK(proxyEntryType,StringMap{"success":"success","body":body},true)
 }
 
 func (p *ProxyController) ReverseConfig() {
@@ -23,8 +23,8 @@ func (p *ProxyController) ReverseConfig() {
 	module := fmt.Sprintf("self::%s",p.Ctx.Input.Param(":module"))
 	body,err := common.Reverse(p.Ctx.Input,module)
 	if err != nil{
-		p.JsonError(proxyEntryType,err.Error(),StringMap{"result":StringMap{"success":"fail"}},true)
+		p.JsonError(proxyEntryType,err.Error(),StringMap{"success":"fail"},true)
 	}
-	p.JsonOK(proxyEntryType,StringMap{"result":StringMap{"success":"success","body":body}},true)
+	p.JsonOK(proxyEntryType,StringMap{"success":"success","body":body},true)
 }
 
